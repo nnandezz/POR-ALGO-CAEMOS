11add34d52a28c88b20def2f77d8db96e6c29ed7
Nandez <alvaroo.fernandezz7@gmail.com>
2026-09-21 13:17:02 +0200

    Keep app cache isolated and cache SF Pro font

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "por-algo-caemos-v3";
+const CACHE_NAME = "por-algo-caemos-v4";
+const CACHE_PREFIX = "por-algo-caemos-";
 
 const ARCHIVOS = [
     "./",
@@ -6,57 +7,34 @@ const ARCHIVOS = [
     "./style.css",
     "./script.js",
     "./manifest.json",
-    "./favicon.png"
+    "./favicon.png",
+    "./SF-Pro.ttf"
 ];
 
 self.addEventListener("install", event => {
-
     self.skipWaiting();
 
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(ARCHIVOS))
-
+            .then(cache => cache.addAll(ARCHIVOS))
     );
-
 });
 
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
-        caches.keys().then(keys => {
-
-            return Promise.all(
-
-                keys.map(key => {
-
-                    if(key !== CACHE_NAME){
-
-                        return caches.delete(key);
-
-                    }
-
-                })
-
-            );
-
-        })
-
+        caches.keys().then(keys => Promise.all(
+            keys
+                .filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+        ))
     );
 
     self.clients.claim();
-
 });
 
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
-
+            .then(response => response || fetch(event.request))
     );
-
 });
