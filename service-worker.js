665171837b1d0985733c3138d38270e6116e7ce3
Nandez <alvaroo.fernandezz7@gmail.com>
2026-09-21 13:20:04 +0200

    Use one root service worker for the complete PWA

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,49 @@
-const CACHE_NAME = "por-algo-caemos-v4";
+const CACHE_NAME = "por-algo-caemos-v5";
 const CACHE_PREFIX = "por-algo-caemos-";
 
 const ARCHIVOS = [
     "./",
     "./index.html",
+    "./juegos.html",
+    "./tienda.html",
+    "./perfil.html",
     "./style.css",
     "./script.js",
+    "./perfil.js",
+    "./tienda.js",
     "./manifest.json",
     "./favicon.png",
-    "./SF-Pro.ttf"
+    "./SF-Pro.ttf",
+    "./spiderflappy.html",
+    "./spiderflappy.css",
+    "./spiderflappy.js",
+    "./spiderflappy-manifest.json",
+    "./spiderflappy.png",
+    "./spider-art.png",
+    "./spidercaida.png",
+    "./spidermedio.png",
+    "./spidersubida.png",
+    "./spidermusic.mp3",
+    "./elbaifo.html",
+    "./elbaifo.css",
+    "./elbaifo.js",
+    "./elbaifo-manifest.json",
+    "./elbaifo.png",
+    "./elbaifo2.png",
+    "./elbaifo3.png",
+    "./elbaifo4.png",
+    "./elbaifo_caida.png",
+    "./elbaifo_salto.png",
+    "./elbaiforun.png",
+    "./baifomusic.mp3",
+    "./canarias.png",
+    "./canarias_icono.png",
+    "./ciudad_dia.png",
+    "./ciudad_noche.png",
+    "./columbia.png",
+    "./edificio_dia.png",
+    "./edificio_noche.png",
+    "./spiderman.png"
 ];
 
 self.addEventListener("install", event => {
