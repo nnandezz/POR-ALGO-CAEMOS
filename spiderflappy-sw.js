bf708234fb93f5a6252d5d1b25d22cb2aaccb6b7
Nandez <alvaroo.fernandezz7@gmail.com>
2026-09-21 13:17:17 +0200

    Keep Spider-Flappy cache isolated and cache SF Pro font

--- spiderflappy-sw.js
+++ spiderflappy-sw.js
@@ -1,88 +1,39 @@
-const CACHE_NAME = "spider-flappy-v2";
-
+const CACHE_NAME = "spider-flappy-v3";
+const CACHE_PREFIX = "spider-flappy-";
 
 const ARCHIVOS = [
-
     "./spiderflappy.html",
-
     "./spiderflappy.css",
-
     "./spiderflappy.js",
-
     "./spiderflappy-manifest.json",
-
-    "./spider-art.png"
-
+    "./spider-art.png",
+    "./SF-Pro.ttf"
 ];
 
-
-
 self.addEventListener("install", event => {
-
+    self.skipWaiting();
 
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-
-        .then(cache => {
-
-            return cache.addAll(ARCHIVOS);
-
-        })
-
+            .then(cache => cache.addAll(ARCHIVOS))
     );
-
-
 });
-
-
 
 self.addEventListener("activate", event => {
-
-
     event.waitUntil(
-
-        caches.keys()
-
-        .then(keys => {
-
-
-            return Promise.all(
-
-                keys
-
-                .filter(key => key !== CACHE_NAME)
-
+        caches.keys().then(keys => Promise.all(
+            keys
+                .filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
                 .map(key => caches.delete(key))
-
-            );
-
-
-        })
-
+        ))
     );
 
-
+    self.clients.claim();
 });
 
-
-
 self.addEventListener("fetch", event => {
-
-
     event.respondWith(
-
         caches.match(event.request)
-
-        .then(response => {
-
-
-            return response || fetch(event.request);
-
-
-        })
-
+            .then(response => response || fetch(event.request))
     );
-
-
 });
