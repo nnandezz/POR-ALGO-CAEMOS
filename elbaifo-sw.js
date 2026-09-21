b29ebc2ef0619373d5b99548546e3e5bfd5104d3
Nandez <alvaroo.fernandezz7@gmail.com>
2026-09-21 13:16:45 +0200

    Keep El Baifo Run cache isolated and cache SF Pro font

--- elbaifo-sw.js
+++ elbaifo-sw.js
@@ -1,68 +1,40 @@
-const CACHE_NAME = "el-baifo-run-v1";
+const CACHE_NAME = "el-baifo-run-v2";
+const CACHE_PREFIX = "el-baifo-run-";
 
 const ARCHIVOS = [
-
     "./elbaifo.html",
-
     "./elbaifo.css",
-
     "./elbaifo.js",
-
     "./elbaifo-manifest.json",
-
     "./elbaifo.png",
-
-    "./baifomusic.mp3"
-
+    "./baifomusic.mp3",
+    "./SF-Pro.ttf"
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
 
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-        .then(cache => {
-
-            return cache.addAll(ARCHIVOS);
-
-        })
-
+            .then(cache => cache.addAll(ARCHIVOS))
     );
-
 });
 
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
-        caches.keys()
-        .then(keys => {
-
-            return Promise.all(
-
-                keys
-                .filter(key => key !== CACHE_NAME)
+        caches.keys().then(keys => Promise.all(
+            keys
+                .filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
                 .map(key => caches.delete(key))
-
-            );
-
-        })
-
+        ))
     );
 
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
         caches.match(event.request)
-        .then(response => {
-
-            return response || fetch(event.request);
-
-        })
-
+            .then(response => response || fetch(event.request))
     );
-
 });
